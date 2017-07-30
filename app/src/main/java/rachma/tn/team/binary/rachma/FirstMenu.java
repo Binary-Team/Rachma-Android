0f307cec9176655898cfccb1de8c6b37084868b4
Mohamed Amine Znaidi <mohamedamine.znaidi@esprit.tn>
2017-07-30 20:11:03 +0100

    add first Menu

---
+++ app/src/main/java/rachma/tn/team/binary/rachma/FirstMenu.java
@@ -0,0 +1,14 @@
+package rachma.tn.team.binary.rachma;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+//@Author Mohamed Amine Znaidi
+public class FirstMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_menu);
+    }
+}
