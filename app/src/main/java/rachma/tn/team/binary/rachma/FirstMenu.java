387b9a294db084b0c04649d4664918f7b4450a27
Mohamed Amine Znaidi <mohamedamine.znaidi@esprit.tn>
2017-07-31 00:10:00 +0100

    First Menu completed

--- app/src/main/java/rachma/tn/team/binary/rachma/FirstMenu.java
+++ app/src/main/java/rachma/tn/team/binary/rachma/FirstMenu.java
@@ -1,7 +1,10 @@
 package rachma.tn.team.binary.rachma;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 //@Author Mohamed Amine Znaidi
 public class FirstMenu extends AppCompatActivity {
@@ -10,5 +13,24 @@ public class FirstMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_menu);
+        ImageButton rummyBt = (ImageButton) findViewById(R.id.rummy);
+        ImageButton beloteBt = (ImageButton) findViewById(R.id.belote);
+
+        rummyBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent rummysetting = new Intent(getApplication(), RummySetting.class);
+                startActivity(rummysetting);
+            }
+        });
+
+        beloteBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent belotesetting = new Intent(getApplication(), RummySetting.class);
+                startActivity(belotesetting);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/rachma/tn/team/binary/rachma/RummySetting.java
@@ -0,0 +1,13 @@
+package rachma.tn.team.binary.rachma;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RummySetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rummy_setting);
+    }
+}
