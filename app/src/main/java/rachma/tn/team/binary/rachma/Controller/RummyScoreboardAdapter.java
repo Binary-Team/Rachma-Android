905b531083293601a694a7561dc22bdd9921e35a
Marwen Doukh <marwen.doukh@gmail.com>
2017-08-01 01:21:26 +0100

    score list

---
+++ app/src/main/java/rachma/tn/team/binary/rachma/Controller/RummyScoreboardAdapter.java
@@ -0,0 +1,56 @@
+package rachma.tn.team.binary.rachma.Controller;
+
+/**
+ * Created by Marwen on 01/08/17.
+ */
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import rachma.tn.team.binary.rachma.R;
+
+
+public class RummyScoreboardAdapter extends RecyclerView.Adapter<RummyScoreboardAdapter.MyViewHolder> {
+
+
+    private List<String> scoreList;
+
+    public RummyScoreboardAdapter(List<String> scoreList) {
+        this.scoreList = scoreList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.scoreboard_list_row, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.score.setText(scoreList.get(position));
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return scoreList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView score;
+
+        public MyViewHolder(View view) {
+            super(view);
+
+            score = (TextView) view.findViewById(R.id.score);
+
+        }
+    }
+}
--- app/src/main/java/rachma/tn/team/binary/rachma/RummyScoreboard.java
+++ app/src/main/java/rachma/tn/team/binary/rachma/RummyScoreboard.java
@@ -2,15 +2,67 @@ package rachma.tn.team.binary.rachma;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rachma.tn.team.binary.rachma.Controller.RummyScoreboardAdapter;
 
 
 //Author Marwen Doukh
 
 public class RummyScoreboard extends AppCompatActivity {
 
+    Integer playersNumber;
+
+    List<String> scoreList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private RummyScoreboardAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rummy_scoreboard);
+
+        // get players number
+        playersNumber = getIntent().getExtras().getInt("playersNumber");
+
+        // RecyclerView
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_scoreboard);
+        mAdapter = new RummyScoreboardAdapter(scoreList);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        // add score
+        Button addScore = (Button) findViewById(R.id.addScore);
+        addScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scoreList.add("score1");
+                mAdapter.notifyDataSetChanged();
+            }
+        });
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
