9340c88ba3f5ecc2481769b336c7b1c5210ea2de
Marwen Doukh <marwen.doukh@gmail.com>
2017-09-12 22:25:07 +0100

    Add Hamburger icon to actionbar (issue #30)

--- app/src/main/java/rachma/tn/team/binary/rachma/MainActivity.java
+++ app/src/main/java/rachma/tn/team/binary/rachma/MainActivity.java
@@ -1,12 +1,15 @@
 package rachma.tn.team.binary.rachma;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,6 +19,7 @@ import rachma.tn.team.binary.rachma.rummy.RummySetting;
 public class MainActivity extends AppCompatActivity {
 
     TextView rummy, belote, settings;
+    ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
 
     @Override
@@ -23,11 +27,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                toolbar, R.string.save_score, R.string.score) {
+
+            /**
+             * Called when a drawer has settled in a completely closed state.
+             */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            /**
+             * Called when a drawer has settled in a completely open state.
+             */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
         rummy = (TextView) findViewById(R.id.rummy_navigation_drawer);
         belote = (TextView) findViewById(R.id.belote_navigation_drawer);
         settings = (TextView) findViewById(R.id.settings_navigation_drawer);
-
 
         rummy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,5 +97,17 @@ public class MainActivity extends AppCompatActivity {
                 mDrawerLayout.closeDrawer(navigationView);
             }
         });
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 }
