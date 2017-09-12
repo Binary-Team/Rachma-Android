78bab1175052ab2276c10ad960b7cae2c5715f0c
Marwen Doukh <marwen.doukh@gmail.com>
2017-09-12 23:13:27 +0100

    Update navigation drawer  (issue #30)

--- app/src/main/java/rachma/tn/team/binary/rachma/ChooseGame.java
+++ app/src/main/java/rachma/tn/team/binary/rachma/ChooseGame.java
@@ -2,27 +2,37 @@ package rachma.tn.team.binary.rachma;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import rachma.tn.team.binary.rachma.belote.BeloteSetting;
 import rachma.tn.team.binary.rachma.rummy.RummySetting;
 
 //@Author Mohamed Amine Znaidi
-public class ChooseGame extends AppCompatActivity {
+public class ChooseGame extends Fragment {
+
+    CardView rummyBt, beloteBt;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View mView = inflater.inflate(R.layout.activity_choose_game, null);
+        rummyBt = (CardView) mView.findViewById(R.id.rummy);
+        beloteBt = (CardView) mView.findViewById(R.id.belote);
+        return mView;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_game);
-        CardView rummyBt = (CardView) findViewById(R.id.rummy);
-        CardView beloteBt = (CardView) findViewById(R.id.belote);
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
         rummyBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent rummysetting = new Intent(getApplication(), RummySetting.class);
+                Intent rummysetting = new Intent(getActivity(), RummySetting.class);
                 startActivity(rummysetting);
             }
         });
@@ -30,7 +40,7 @@ public class ChooseGame extends AppCompatActivity {
         beloteBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent belotesetting = new Intent(getApplication(), BeloteSetting.class);
+                Intent belotesetting = new Intent(getActivity(), BeloteSetting.class);
                 startActivity(belotesetting);
             }
         });
--- app/src/main/java/rachma/tn/team/binary/rachma/MainActivity.java
+++ app/src/main/java/rachma/tn/team/binary/rachma/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,7 +19,7 @@ import rachma.tn.team.binary.rachma.rummy.RummySetting;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout rummy, belote, settings;
+    LinearLayout rummy, belote, settings, home;
     ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
 
@@ -26,6 +27,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rummy = (LinearLayout) findViewById(R.id.rummy_navigation_drawer);
+        belote = (LinearLayout) findViewById(R.id.belote_navigation_drawer);
+        settings = (LinearLayout) findViewById(R.id.settings_navigation_drawer);
+        home = (LinearLayout) findViewById(R.id.home_navigation_drawer);
+
+
+        // insert choose game fragment
+        Fragment fragment = new ChooseGame();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view_drawer);
+        setActiveMenu(home);
+        setInctiveMenu(rummy);
+        setInctiveMenu(settings);
+        setInctiveMenu(belote);
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
@@ -54,9 +74,6 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout.setDrawerListener(mDrawerToggle);
 
 
-        rummy = (LinearLayout) findViewById(R.id.rummy_navigation_drawer);
-        belote = (LinearLayout) findViewById(R.id.belote_navigation_drawer);
-        settings = (LinearLayout) findViewById(R.id.settings_navigation_drawer);
 
         rummy.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,6 +87,9 @@ public class MainActivity extends AppCompatActivity {
                         .commit();
                 NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view_drawer);
                 mDrawerLayout.closeDrawer(navigationView);
+                setActiveMenu(rummy);
+                setInctiveMenu(belote);
+                setInctiveMenu(home);
             }
         });
 
@@ -85,6 +105,9 @@ public class MainActivity extends AppCompatActivity {
                         .commit();
                 NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view_drawer);
                 mDrawerLayout.closeDrawer(navigationView);
+                setActiveMenu(belote);
+                setInctiveMenu(rummy);
+                setInctiveMenu(home);
             }
         });
 
@@ -95,8 +118,39 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
                 NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view_drawer);
                 mDrawerLayout.closeDrawer(navigationView);
+                setActiveMenu(home);
+                setInctiveMenu(rummy);
+                setInctiveMenu(belote);
             }
         });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Fragment fragment = new ChooseGame();
+                // Insert the fragment by replacing any existing fragment
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.content_frame, fragment)
+                        .commit();
+                NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view_drawer);
+                mDrawerLayout.closeDrawer(navigationView);
+                setActiveMenu(home);
+                setInctiveMenu(rummy);
+                setInctiveMenu(belote);
+            }
+        });
+
+
+    }
+
+    private void setActiveMenu(LinearLayout activeLinearLayout) {
+        activeLinearLayout.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark));
+    }
+
+    private void setInctiveMenu(LinearLayout InactiveLinearLayout) {
+        InactiveLinearLayout.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
     }
 
     @Override
